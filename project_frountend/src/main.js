23075519e1eef9cbf0c1e42b1777439021a22d45
Daniela Sandoval <daniela.sandoval820@gmail.com>
2019-07-08 23:13:24 -0400

    basic skeleton

--- project_frountend/src/main.js
+++ project_frountend/src/main.js
@@ -1,1 +1,29 @@
-console.log("Hi");
+const theApp = document.querySelector('#the_app')
+const graveYard = document.querySelector(".graveyard");
+
+// ---------------- EVENT LISTENERS WELCOME ----------------
+document.addEventListener("DOMContentLoaded", () => {
+  const newTomb = document.querySelector(".js-btn");
+  const welcomeForm = document.querySelector('.the_form');
+
+  newTomb.addEventListener("click", addTomb)
+  welcomeForm.addEventListener("submit", secondStage)
+
+})
+
+// ---------------- DOM LOGIC WELCOME ----------------
+function secondStage(event) {
+  event.preventDefault()
+  event.target.parentElement.className += " hidden"
+  theApp.className = ""
+}
+
+function addTomb() {
+  graveYard.innerHTML +=  `
+   <div class="tomb">BOO</div>
+  `
+  anime({
+    targets: '.tomb',
+    translateY: -30
+  });
+}
