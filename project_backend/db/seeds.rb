99501052fa3e59f41e72d032ef114f9135fc61b6
Daniela Sandoval <daniela.sandoval820@gmail.com>
2019-07-12 11:02:37 -0400

    v.1 graveyard

--- project_backend/db/seeds.rb
+++ project_backend/db/seeds.rb
@@ -5,24 +5,37 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-# fan = User.create(name: "Fan")
 
-boo = Story.create(title: "Sup secry", content: "Boo", user_id: 1)
+og_user = User.create(name: "daniela");
 
-# Story.create(title: "Not Sup secry", content: "Boo", user_id: 3)
-# Story.create(title: "Sup Sup secry", content: "NUUU", user_id: 1)
-# Story.create(title: "Sup secry", content: "WUUUU", user_id: 1)
+invis_bug = Story.create(title: "The Invisible Bug", content:"No matter where I look...I could not find the bug. I know it's there. Watching me, watching me type my code. Savoring the moment when the satisfication of a working code is broken my red. All I see is errors. But no bug in sight. Where are you. I will find you bug.", like_count: 0, user_id: og_user.id)
 
-tag_1 = Tag.create(name: "Blood")
+the_comp_ghost = Story.create(title: "Ghost Coder", content: "Every night at 3 am, there's clicking from the living room. At first I thought it was my cats, but one night I got up and found my cats near my feet. My cats also don't have fingers. I slowly walked to the living room to see my computer wide open, a freshly made cup of coffee, and a web page with a single p tag saying.....hello from the underworld.", like_count: 0, user_id: og_user.id)
 
-story_tag = StoryTag.create(story: boo, tag: tag_1)
-#
-# comment_1 = Comment.create(content: "nice story", user: fan, story: boo)
-#
-# like_1 = Like.create(user: fan, story: boo)
-# like_2 = Like.create(user_id: 1, story_id: 1)
-# like_3 = Like.create(user_id: 1, story_id: 1)
-# like_4 = Like.create(user_id: 1, story_id: 1)
+hands = Story.create(title:"Helpful Hand", content: "I think my body isn't mine. Sometimes I wake up holding something. Sometimes it was a book, a pen, a lamp, but today...tonight someone's computer. I am typing this story, but the moment I stop my body wants to chop me off. I think I need a knew body.", like_count: 0, user_id: og_user.id)
+
+signal = Story.create(title: "No Signal", content: "There's something in my basement, but everytime I turn on the light I hear a hiss and there's nothing there. When I stand at the top of the staircase I see one small ember, but so light I'm afraid I'm losing my mind. Today I took a picture with flash and shut the door. When I looked at it... those small embers I saw was really the lights from a cellphone floating instead a demon's body.", like_count: 0, user_id: og_user.id)
+
+loans = Story.create(title: "The Carrier", content: "It comes right on time. It drops off a letter and leaves with soft foot steps. What it brings is much worse. I'm afriad to even look at it and I dread when it comes. It's my student loans letter, back to haunt me.", like_count: 0, user_id: og_user.id)
+
+ghost_tag = Tag.create(name: "Ghost")
+funny_tag = Tag.create(name: "Funny")
+creepy_tag = Tag.create(name: "Creepy")
+creature_tag = Tag.create(name: "Creature")
+
+invis_bug_tag = StoryTag.create(story_id: invis_bug.id, tag_id: ghost_tag.id)
+
+invis_bug_tag2 = StoryTag.create(story_id: invis_bug.id, tag_id: creepy_tag.id)
+
+the_comp_ghost_tag = StoryTag.create(story_id: the_comp_ghost.id, tag_id: ghost_tag.id)
+
+the_comp_ghost_tag2 = StoryTag.create(story_id: the_comp_ghost.id, tag_id: funny_tag.id)
+
+hands_tag = StoryTag.create(story_id: hands.id, tag_id: creepy_tag.id)
+
+signal_tag = StoryTag.create(story_id: signal.id, tag_id: creature_tag.id)
+
+loans = StoryTag.create(story_id: loans.id, tag_id: funny_tag.id)
 
 
 puts "good seeding"
